f7c7cffa734156cf583a3c659fb53985296f3ee7
Abhishek Sharma <sharmaash876@gmail.com>
2020-04-07 14:59:04 +0530

    updated app.js

--- app.js
+++ app.js
@@ -60,8 +60,8 @@ passport.deserializeUser(function(id, done) {
 });
 
 passport.use(new GoogleStrategy({
-    clientSecret: process.env.CLIENT_SECRET,
     clientID: process.env.CLIENT_ID,
+    clientSecret: process.env.CLIENT_SECRET,
     callbackURL: "http://localhost:3000/auth/google/secrets",
     userProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo"
   },
